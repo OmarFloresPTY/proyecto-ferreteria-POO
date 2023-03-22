5d659e1f33321791a7d1ecf8931a33543aa47264
Omar Flores <oafo2699@gmail.com>
2023-03-22 15:24:42 -0500

    agreamos unas instancias y realizamos composicion entre categoria y producto, eliminamos la herencia entre ellos tambien

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    pass
+from producto import Producto
+from departamento import Departamento
 
+def main():
+    construccion = Departamento("D01","Construcción")
+    cemento = Producto(
+        id = "P01",
+        nombre_producto="Cemento Argos",
+        precio_producto=7.90,
+        disponibilidad=10,
+        marca = "Argos",
+        departamento = construccion
+    )
+    cemento.descripcion = "Cemento utilizado para la construcción en general"
+    cemento.PrintDataProducto()
+
+    
 if __name__ == "__main__":
     main()
--- producto.py
+++ producto.py
@@ -1,11 +1,19 @@
 from departamento import Departamento
 
 class Producto(Departamento):
-    def __init__(self, id_dep, nombre_departamento, id, nombre_producto, precio_producto, disponibilidad, marca) -> None:
-        super().__init__(id_dep, nombre_departamento)
+    
+    def __init__(self,id, nombre_producto, precio_producto, disponibilidad, marca, departamento) -> None:
         self.id = id
         self.nombre_producto = nombre_producto
         self.precio_producto = precio_producto
         self.disponibilidad = disponibilidad
         self.marca = marca
+        self.departamento = departamento
         self.descripcion = str
+    
+    def PrintDataProducto(self):
+        print("IMPRESION DE DATOS DEL PRODUCTO")
+        print("---------------------------------")
+        print(f'id = {self.id} --> nombre producto = {self.nombre_producto} --> precio = {self.precio_producto}')
+        print(f'disponibilidad = {self.disponibilidad} --> marca = {self.marca}')
+        print(f'Departamento = {self.departamento.nombre_departamento} --> id Departamento = {self.departamento.id_dep}')
